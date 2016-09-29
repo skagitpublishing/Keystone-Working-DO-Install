56bd387c804b5450822fe3e825b3e6bf9d28fac7
Chris Troutner <chris.troutner@gmail.com>
2016-09-29 16:29:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KeystoneJS v4.0 Beta Fork
+This repository contains a working installation of Keystone v4.0 Beta, including the node_modules directory. The purpose
+is to provide a resource for people having a hard time installing KeystoneJS using the Yeoman generator or other methods.
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+var middleware = require('./middleware');
+var importRoutes = keystone.importer(__dirname);
+
+// Common Middleware
+keystone.pre('routes', middleware.initLocals);
+keystone.pre('render', middleware.flashMessages);
+
+// Import Route Controllers
+var routes = {
+  views: importRoutes('./views'),
+  api: importRoutes('./api')      // ADD THIS LINE TOO!
+};
+
+// Setup Route Bindings
+exports = module.exports = function (app) {
+  // Views
+  app.get('/', routes.views.index);
+  app.get('/blog/:category?', routes.views.blog);
+  app.get('/blog/post/:post', routes.views.post);
+  app.get('/gallery', routes.views.gallery);
+  app.all('/contact', routes.views.contact);
+
+  // COPY THE CODE FROM HERE...
+  //File Upload Route
+  app.get('/api/fileupload/list', keystone.middleware.api, routes.api.fileupload.list);
+  app.get('/api/fileupload/:id', keystone.middleware.api, routes.api.fileupload.get);
+  app.all('/api/fileupload/:id/update', keystone.middleware.api, routes.api.fileupload.update);
+  app.all('/api/fileupload/create', keystone.middleware.api, routes.api.fileupload.create);
+  app.get('/api/fileupload/:id/remove', keystone.middleware.api, routes.api.fileupload.remove);
+  // ...TO HERE.
+
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+  // app.get('/protected', middleware.requireUser, routes.views.protected);
+
+};
